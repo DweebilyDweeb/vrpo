4a962b8bb574ee7621763519e7a61e1d50bce993
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-06 12:25:54 +0800

    Cannonball collision reaction, basic Player mechanics.

---
+++ VRPO/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private int HP;
+
+    public int GetHP() { return HP; }
+    public void SetHP(int newHP) { HP = newHP; }
+    public void AddHP(int value) { HP += value; Debug.Log("Player HP = " + HP); }
+	// Use this for initialization
+	void Start () {
+        HP = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
