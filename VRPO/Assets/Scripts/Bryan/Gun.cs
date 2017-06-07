246e00faaaa705c4c54c7778d02efa8999f84318
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-07 17:55:56 +0800

    Set-up SteamVR Camera properly

--- VRPO/Assets/Scripts/Bryan/Gun.cs
+++ VRPO/Assets/Scripts/Bryan/Gun.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour {
-
+public class Gun : MonoBehaviour 
+{
+    
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		
 	}
 }
