da408418c730904578ab0f4932100ddef0ea9f71
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-08 15:44:52 +0800

    Allows switching of gun firing modes

--- VRPO/Assets/Scripts/Bryan/Gun.cs
+++ VRPO/Assets/Scripts/Bryan/Gun.cs
@@ -26,56 +26,65 @@ public class Gun : MonoBehaviour
             gameObject.GetComponent<SteamVR_LaserPointer>().holder.SetActive(true);
         #endregion
 
-        #region Charge and fire
-        //try
-        //{
-        //    if (controller.device.GetPressDown(controller.triggerButton))
-        //    {
-        //        if (gunChargeTimer > 0)
-        //            gunChargeTimer -= Time.deltaTime;
-        //        else
-        //            gunCharged = true;
-        //    }
-        //    else
-        //    {
-        //        if (gunChargeTimer < 2 && !gunCharged)
-        //            gunChargeTimer += Time.deltaTime;
-        //    }
-        //    if (controller.device.GetPressUp(controller.triggerButton) && gunCharged)
-        //    {
-        //        Fire();
-        //        gunChargeTimer = 2.0f;
-        //        gunCharged = false;
-        //    }
-        //}
-        //catch (Exception e) { Debug.LogError(e); }
-        #endregion
-
-        #region Hold Down Track Pad and Fire
-        try
+        switch(PlayerPrefs.GetInt("FiringMode", 1))
         {
-            if (controller.device.GetPress(controller.touchPad))
-                gunCharged = true;
-            else
-                gunCharged = false;
+            case 0:
+                #region Charge and fire
+                try
+                {
+                    if (controller.device.GetPressDown(controller.triggerButton))
+                    {
+                        if (gunChargeTimer > 0)
+                            gunChargeTimer -= Time.deltaTime;
+                        else
+                            gunCharged = true;
+                    }
+                    else
+                    {
+                        if (gunChargeTimer < 2 && !gunCharged)
+                            gunChargeTimer += Time.deltaTime;
+                    }
+                    if (controller.device.GetPressUp(controller.triggerButton) && gunCharged)
+                    {
+                        Fire();
+                        gunChargeTimer = 2.0f;
+                        gunCharged = false;
+                    }
+                }
+                catch (Exception e) { Debug.LogError(e); }
+                break;
+                #endregion
+            case 1:
+                #region Hold Down Track Pad and Fire
+                try
+                {
+                    if (controller.device.GetPress(controller.touchPad))
+                        gunCharged = true;
+                    else
+                        gunCharged = false;
 
-            if(controller.device.GetPressDown(controller.triggerButton) && gunCharged)
-            {
-                Fire();
-                gunCharged = false;
-            }
+                    if (controller.device.GetPressDown(controller.triggerButton) && gunCharged)
+                    {
+                        Fire();
+                        gunCharged = false;
+                    }
+                }
+                catch (Exception e) { Debug.LogError(e); }
+                break;
+                #endregion
+            case 2:
+                #region Fire at will
+                if (!gunCharged)
+                    gunCharged = true;
+                try
+                {
+                    if (controller.device.GetPressDown(controller.triggerButton))
+                        Fire();
+                }
+                catch (Exception e) { Debug.LogError(e); }
+                break;
+                #endregion
         }
-        catch (Exception e) { Debug.LogError(e); }
-        #endregion
-
-        //#region Fire at will
-        //try
-        //{
-        //    if (controller.device.GetPressDown(controller.triggerButton))
-        //        Fire();
-        //}
-        //catch(Exception e) { Debug.LogError(e); }
-        //#endregion
     }
 
     private void Fire()
