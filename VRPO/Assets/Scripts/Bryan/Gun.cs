f7a305228067dc0a7d1b50d5a04f1daf45628e0c
SIDM\150553E <wolfyfreakz@hotmail.com>
2017-06-14 13:05:36 +0800

    Muzzle flas particle effects for flintlock pistol

--- VRPO/Assets/Scripts/Bryan/Gun.cs
+++ VRPO/Assets/Scripts/Bryan/Gun.cs
@@ -5,101 +5,21 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour 
 {
-    private ViveController controller;
-    private float gunChargeTimer = 2.0f;
-    private bool gunCharged = false;
+    private ViveController_Manager controller;
 	// Use this for initialization
 	void Start() 
     {
-        controller = new ViveController();
-        controller.trackedObject = gameObject.GetComponent<SteamVR_TrackedObject>();
+        controller = gameObject.GetComponentInParent<ViveController_Manager>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        controller.Update();
-        #region Laser Pointer
-        if(!gunCharged)
-            gameObject.GetComponent<SteamVR_LaserPointer>().holder.SetActive(false);
-        else
-            gameObject.GetComponent<SteamVR_LaserPointer>().holder.SetActive(true);
-        #endregion
-
-        switch(PlayerPrefs.GetInt("FiringMode", 1))
-        {
-            case 0:
-                #region Charge and fire
-                try
-                {
-                    if (controller.device.GetPressDown(controller.triggerButton))
-                    {
-                        if (gunChargeTimer > 0)
-                            gunChargeTimer -= Time.deltaTime;
-                        else
-                            gunCharged = true;
-                    }
-                    else
-                    {
-                        if (gunChargeTimer < 2 && !gunCharged)
-                            gunChargeTimer += Time.deltaTime;
-                    }
-                    if (controller.device.GetPressUp(controller.triggerButton) && gunCharged)
-                    {
-                        Fire();
-                        gunChargeTimer = 2.0f;
-                        gunCharged = false;
-                    }
-                }
-                catch (Exception e) { Debug.LogError(e); }
-                break;
-                #endregion
-            case 1:
-                #region Hold Down Track Pad and Fire
-                try
-                {
-                    if (controller.device.GetPress(controller.touchPad))
-                        gunCharged = true;
-                    else
-                        gunCharged = false;
-
-                    if (controller.device.GetPressDown(controller.triggerButton) && gunCharged)
-                    {
-                        Fire();
-                        gunCharged = false;
-                    }
-                }
-                catch (Exception e) { Debug.LogError(e); }
-                break;
-                #endregion
-            case 2:
-                #region Fire at will
-                if (!gunCharged)
-                    gunCharged = true;
-                try
-                {
-                    if (controller.device.GetPressDown(controller.triggerButton))
-                        Fire();
-                }
-                catch (Exception e) { Debug.LogError(e); }
-                break;
-                #endregion
-        }
+        
     }
 
-    private void Fire()
+    public void Fire()
     {
-        Ray raycast = new Ray(transform.position, transform.forward);
-        RaycastHit _hit;
-        if (Physics.Raycast(raycast, out _hit))
-        {
-            GameObject collide = _hit.collider.gameObject;
-            switch(collide.tag)
-            {
-                case "Cannonball":
-                    Destroy(collide);
-                    break;
-            }
-        }
+        controller.FireGun();
     }
 }
