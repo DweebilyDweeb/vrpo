72b257c7096917932c23f72684eb0635c1addbed
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-19 14:17:41 +0800

    Prototype piranha spawn and jumping, reorganized files.

---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Piranha.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piranha : MonoBehaviour 
+{
+    private GameObject target;
+    private float distance;
+    private float lifetime = 5.0f;
+    private bool isRightSide, inJumpRange, hasJumped;
+    
+	// Use this for initialization
+    
+    void Start () 
+    {
+        target = GameObject.FindGameObjectWithTag("Boat");
+        inJumpRange = false;
+        hasJumped = false;
+	}
+
+    public void InitSide(bool right)
+    {
+        isRightSide = right;
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        distance = new Vector3(target.transform.position.x - transform.position.x, 0, target.transform.position.z - transform.position.z).magnitude;
+        //Debug.Log("distance from boat: " + distance);
+        if (distance < 8 && !inJumpRange && !hasJumped)
+            inJumpRange = true;
+
+        if(inJumpRange)
+        {
+            if (!hasJumped)
+            {
+                hasJumped = true;
+                transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(-30, transform.localEulerAngles.y, 0), 1.0f);
+            }
+            else
+            {
+                Vector3 newRot = transform.localEulerAngles;
+                newRot.x += Time.deltaTime * 20;
+                transform.localRotation = Quaternion.Euler(newRot);
+            }
+        }
+
+        transform.position += transform.forward * Time.deltaTime * 5;
+
+        if (lifetime > 0)
+            lifetime -= Time.deltaTime;
+        else
+            Destroy(gameObject);
+	}
+}
---
+++ VRPO/Assets/Scripts/Bryan/Enemies/PiranhaSpawn.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiranhaSpawn : MonoBehaviour 
+{
+    public float spawnRadius;
+    public int piranhaToSpawn;
+    private Collider triggerZone;
+	// Use this for initialization
+	void Start () 
+    {
+        triggerZone = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Boat")
+        {
+            for(int spawnCount = 0; spawnCount < piranhaToSpawn; spawnCount++)
+            {
+                bool side = HelperFunctions.RandomBool();
+                Vector3 spawnPos = Vector3.zero;
+                Vector3 eulerRot;
+                if(side) //right
+                {
+                    spawnPos.x += spawnRadius;
+                    spawnPos.z += Random.Range(-3, 3);
+                    eulerRot = new Vector3(0, -90, 0);
+                }
+                else //left
+                {
+                    spawnPos.x -= spawnRadius;
+                    spawnPos.z += Random.Range(-3, 3);
+                    eulerRot = new Vector3(0, 90, 0);
+                }
+                GameObject piranha = Instantiate(Resources.Load<GameObject>("Prefabs/Animals/Piranha"), transform);
+                piranha.transform.localPosition = spawnPos;
+                piranha.transform.localEulerAngles = eulerRot;
+                piranha.GetComponent<Piranha>().InitSide(side);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+        Gizmos.matrix = rotationMatrix;
+        Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(Vector3.zero, spawnRadius);
+        Gizmos.DrawLine(new Vector3(-spawnRadius, 0, 0), new Vector3(spawnRadius, 0, 0));
+    }
+}
