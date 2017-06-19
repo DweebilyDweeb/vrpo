bff079e3aa2c8bde0b13de6be5b351b9b4b8d197
SIDM\150553E <wolfyfreakz@hotmail.com>
2017-06-19 16:59:56 +0800

    Half of new level, scripted boat movements and time control singleton

---
+++ VRPO/Assets/Scripts/Malcolm/MoveDestination.cs
@@ -0,0 +1,14 @@
+﻿// MoveDestination.cs
+using UnityEngine;
+
+public class MoveDestination : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
