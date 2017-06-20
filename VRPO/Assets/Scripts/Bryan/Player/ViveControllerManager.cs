b8dce46828d2ce38f862a45b7cff6d8c693e95f3
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-20 10:50:48 +0800

    Replaced "Monster" tag with "Goblin", "Piranha" and "Kraken" tag.

--- VRPO/Assets/Scripts/Bryan/Player/ViveControllerManager.cs
+++ VRPO/Assets/Scripts/Bryan/Player/ViveControllerManager.cs
@@ -167,10 +167,14 @@ public class ViveControllerManager : MonoBehaviour
                     Destroy(collide.gameObject);
                     Instantiate(Resources.Load<GameObject>("Prefabs/Animals/Seagull"));
                     break;
-                case "Monster":
-                    Animator mobAnim = collide.gameObject.GetComponent<Animator>();
-                    try { mobAnim.SetTrigger("Death"); }
-                    catch (Exception e) { Debug.LogError("Could not find Trigger 'Death' in " + collide.name + ", Exception: " + e); }
+                case "Goblin":
+                    try
+                    {
+                        Animator mobAnim = collide.gameObject.GetComponent<Animator>();
+                        try { mobAnim.SetTrigger("Death"); }
+                        catch (Exception e) { Debug.LogError("Could not find Trigger 'Death' in " + collide.name + ", Exception: " + e); }
+                    }
+                    catch (Exception e) { Debug.LogError("Unable to find Animator component in " + collide.name + ", Exception: " + e); }                    
                     break;
             }
         }
