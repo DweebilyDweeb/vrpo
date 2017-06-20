e1efe60478ccd027999a89e87dc1f57261951fb2
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-20 13:04:21 +0800

    Added Goblin class and scripts

---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour 
+{
+    public enum Goblin_FSM { Idle = 1, Walk, Walk_LTurn, Walk_RTurn, Run, Dash, Unsheathe, Attack, Throw, Hit, Death }
+    protected Goblin_FSM currentState = Goblin_FSM.Idle;
+    protected Animator anim;
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TriggerDeath()
+    {
+        anim.SetTrigger("Death");
+        Debug.Log("Killed goblin");
+    }
+}
---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_LandPeaceful.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_LandPeaceful : Goblin
+{
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
