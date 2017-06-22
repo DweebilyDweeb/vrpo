0c79ccd62a4241786e8d87c3611226bd787f1311
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-22 14:47:42 +0800

    WIP Goblin Swimmer & Goblin Boarder

---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_Boarder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_Boarder : Goblin
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        currentState = Goblin_FSM.Board;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_Swimmer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_Swimmer : Goblin 
+{
+	// Use this for initialization
+	void Start () 
+    {
+        currentState = Goblin_FSM.Swim;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
