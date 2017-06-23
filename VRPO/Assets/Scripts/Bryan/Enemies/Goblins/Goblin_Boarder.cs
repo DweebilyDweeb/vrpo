695cfeb21d49acebeab1a5231e633ba65e88e22a
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-23 10:57:28 +0800

    Resized goblins to proper scale, added boat platform prefab for the players.

--- VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_Boarder.cs
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_Boarder.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Goblin_Boarder : Goblin
 {
     public bool isRightSide;
+    private GameObject boat, player;
 	// Use this for initialization
 	void Start () 
     {
         currentState = Goblin_FSM.Board;
         anim = GetComponent<Animator>();
+        boat = GameObject.FindGameObjectWithTag("Boat");
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
+
+    public void Init(bool isRight)
+    {
+        isRightSide = isRight;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		
 	}
 
