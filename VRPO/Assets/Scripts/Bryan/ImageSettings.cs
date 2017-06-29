8cc6cb1bf64676c9634d18c314dbe3a88f23181f
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-29 13:43:01 +0800

    ImageSettings

---
+++ VRPO/Assets/Scripts/Bryan/ImageSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSettings : MonoBehaviour 
+{
+    private Image thisImage;
+	// Use this for initialization
+	void Start () {
+        thisImage = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        byte r = (byte)Random.Range(0, 255.0f);
+        byte g = (byte)Random.Range(0, 255.0f);
+        byte b = (byte)Random.Range(0, 255.0f);
+        thisImage.color = new Color32(r, g, b, 255);
+	}
+}
