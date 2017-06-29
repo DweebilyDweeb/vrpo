e37a5e753638f71901e6c609a54e7db7aebc8478
SIDM\150553E <wolfyfreakz@hotmail.com>
2017-06-29 13:49:40 +0800

    Lightning

---
+++ VRPO/Assets/Scripts/Malcolm/Lightning.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+
+    float randomX;
+    float randomZ;
+    public GameObject LightningBolt;
+    Quaternion randomRot;
+	// Use this for initialization
+	void Start()
+    {
+        StartCoroutine(Wait(10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        for (int x = 0; x < 1; x = 0)
+        {
+            yield return new WaitForSeconds(seconds); //this will wait 5 seconds
+            SpawnLightning();
+        }
+    }
+
+    private Vector3 RandomPosition()
+    {
+        randomX = UnityEngine.Random.Range(-200,200);
+        randomZ = UnityEngine.Random.Range(-200, 200);
+        return new Vector3(randomX, 0, randomZ);
+    }
+
+    private Quaternion RandomRotation()
+    {
+        randomRot.eulerAngles = new Vector3(0, UnityEngine.Random.Range(-180, 180), 0);
+        return randomRot;
+    }
+
+    private void SpawnLightning()
+    {
+        GameObject lightning = Instantiate(LightningBolt, RandomPosition(), RandomRotation());
+
+    }
+
+}
---
+++ VRPO/Assets/Scripts/Malcolm/LightningFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFade : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Wait(1));
+        Destroy(gameObject,10);
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds); //this will wait 5 seconds
+        transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+}
