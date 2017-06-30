e0bb6e2d5d312a4947f2d426a586e5499dd64bcb
Bryan <150587A@mymail.nyp.edu.sg>
2017-06-30 12:23:03 +0800

    Goblin boat

---
+++ VRPO/Assets/Scripts/Bryan/Enemies/Goblins/Goblin_Paddler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_Paddler : Goblin 
+{
+
+	// Use this for initialization
+	void Start () {
+        anim = transform.parent.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        switch (currentState)
+        {
+            case Goblin_FSM.Death:
+                if (!isDead)
+                {
+                    isDead = true;
+
+                    anim.SetTrigger("Death");
+                }
+                break;
+        }
+	}
+}
