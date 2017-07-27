cc4fa033a5172b814551bd947a60a9a109b70e81
SIDM\150553E <wolfyfreakz@hotmail.com>
2017-07-27 13:06:48 +0800

    ggggggggggggggggggggggggggggggggggggggggggggggggggggggggg

---
+++ VRPO/Assets/StopScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StopScript : MonoBehaviour
+{
+    Vector3 Stop;
+
+    // Use this for initialization
+    void Start()
+    {
+        Stop = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Stop")
+        {
+            Debug.Log("Stop");
+            transform.GetComponent<NavMeshAgent>().isStopped = true;
+            transform.GetComponent<NavMeshAgent>().velocity = Stop;
+        }
+    }
+
+}
